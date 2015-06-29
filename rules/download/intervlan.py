2515d3d7914fada5dc44cb576d3392d155f0f167
Anthony Ruhier <anthony.ruhier@gmail.com>
2015-06-30 01:15:35 +0200

    Switch intervlan to FQCodel
    Latency is much better

--- rules/download/intervlan.py
+++ rules/download/intervlan.py
@@ -3,19 +3,21 @@
 # QoS for upload
 
 from config import INTERFACES
-from built_in_classes import PFIFOClass
+from built_in_classes import FQCodelClass
 from rules.qos_formulas import burst_formula
 
 LAN_IF_SPEED = INTERFACES["lan_if"]["if_speed"]
 
 
-class InterVlan(PFIFOClass):
+class InterVlan(FQCodelClass):
     """
     Intervlan need to be fast
     """
     classid = "1:15"
     rate = LAN_IF_SPEED
-    ceil = LAN_IF_SPEED
     burst = burst_formula(rate) * 3
+    limit = burst
+    interval = 10
+    codel_quantum = 1514
     prio = 0
     mark = 10
